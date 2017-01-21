6d26a867420c8caf1589f68fde28b0df7489bd80
Alex Cordonnier <alexjcord@gmail.com>
2017-01-21 07:23:48 -0500

    Added motor control

--- get_image.py
+++ get_image.py
@@ -2,7 +2,7 @@ from socket import *
 import time
 
 sock = socket(AF_INET, SOCK_STREAM)
-sock.connect(("localhost", 8000))
+sock.connect(("cardshark.local", 8000))
 sock.sendall("img\r\n")
 time.sleep(1)
 ret = sock.recv(1048576)
--- pi/server.py
+++ pi/server.py
@@ -1,6 +1,13 @@
 from socket import *
 from subprocess import call
 from base64 import b64encode
+import RPi.GPIO as GPIO
+from motor import Motor
+
+GPIO.setmode(GPIO.BOARD)
+motors = [Motor([3,5,7,8]), Motor([11,13,15,16]), Motor([19,21,23,24])]
+for m in motors:
+    m.rpm = 1
 
 def take_screenshot():
     print("taking screenshot")
@@ -8,12 +15,12 @@ def take_screenshot():
     with open("capture.jpg", "r") as f:
         return b64encode(f.read())
 
-def move_motor(which_motor, num_steps):
-    # TODO
+def move_motor(which_motor, dest_angle):
+    motors[which_motor].move_to(dest_angle)
     return "done"
 
 sock = socket(AF_INET, SOCK_STREAM)
-sock.bind(("localhost", 8000))
+sock.bind(("cardshark.local", 8000))
 sock.listen(5)
 while True:
     (client, address) = sock.accept()
@@ -33,9 +40,9 @@ while True:
             if query == "img":
                 response = take_screenshot()
             elif len(parsed) == 3 and parsed[0] == "motor":
-                which_motor = parsed[1]
-                num_steps = parsed[2]
-                response = move_motor(which_motor, num_steps)
+                which_motor = int(parsed[1])
+                dest_angle = int(parsed[2])
+                response = move_motor(which_motor, dest_angle)
             else:
                 response = "parse error"
     except IOError:
