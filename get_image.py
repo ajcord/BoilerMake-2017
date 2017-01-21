abc1c41733b84afc53e3e426a90e7296e097986b
Alex Cordonnier <alexjcord@gmail.com>
2017-01-21 06:17:56 -0500

    Should probably check in some code

---
+++ get_image.py
@@ -0,0 +1,9 @@
+from socket import *
+import time
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.connect(("localhost", 8000))
+sock.sendall("img\r\n")
+time.sleep(1)
+ret = sock.recv(1048576)
+print(ret)
---
+++ pi/server.py
@@ -0,0 +1,45 @@
+from socket import *
+from subprocess import call
+from base64 import b64encode
+
+def take_screenshot():
+    print("taking screenshot")
+    call(["raspistill", "-o", "capture.jpg", "-w", "320", "-h", "240", "-n", "-t", "1"])
+    f = open("capture.jpg", "r")
+    b64 = b64encode(f.read())
+    f.close()
+    print(b64)
+    return b64
+
+def move_motor(which_motor, num_steps):
+    # TODO
+    return "done"
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind(("localhost", 8000))
+sock.listen(5)
+while True:
+    (client, address) = sock.accept()
+    print("Accepted client")
+    response = ""
+    try:
+        while True:
+            client.sendall(response + "\r\n")
+            query = client.recv(64)
+            if query.endswith("\r\n"):
+                query = query[:-2]
+            else:
+                response = "parse error"
+                continue
+
+            parsed = query.split(",")
+            if query == "img":
+                response = take_screenshot()
+            elif len(parsed) == 3 and parsed[0] == "motor":
+                which_motor = parsed[1]
+                num_steps = parsed[2]
+                response = move_motor(which_motor, num_steps)
+            else:
+                response = "parse error"
+    except IOError:
+        continue
